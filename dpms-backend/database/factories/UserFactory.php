193c8beb5287048dc112da926c50f100da0cb803
Nauf <naufpsychic@gmail.com>
2025-02-15 17:14:29 +0500

    fix: ensure it's macl.aero email

--- dpms-backend/database/factories/UserFactory.php
+++ dpms-backend/database/factories/UserFactory.php
@@ -23,10 +23,14 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $firstName = fake()->firstName();
+        $lastName = fake()->lastName();
+        $email = strtolower($firstName . '.' . $lastName . '@macl.aero');
+
         return [
-            'first_name' => fake()->firstName(),
-            'last_name' => fake()->lastName(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
